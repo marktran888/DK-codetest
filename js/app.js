e30c3d36f004a20d32b74971b4fb57a8ebd7bd20
Mark Tran <mark.tran@hotmail.co.uk>
2018-05-31 15:36:12 +0100

    added selected toggle and changed event listener to change from click

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 const catergories = {
-  chidrens: {label: 'Children\'s'},
-  crafts: {label: 'Crafts & Hobbies'},
-  education: {label: 'Education & Teachers'},
-  food: {label: 'Food & Drink'},
-  garden: {label: 'Gardening'},
-  health: {label: 'Health & Fitness'},
-  history: {label: 'History & Reference'},
-  lego: {label: 'Lego&reg; Books'},
-  marvel: {label: 'Marvel'},
-  pregnancy: {label: 'Pregnancy & Parenting'},
-  star: {label: 'Star Wars&trade;'},
-  travel: {label: 'Travel'}
+  chidrens: {label: 'Children\'s', selected: false},
+  crafts: {label: 'Crafts & Hobbies', selected: false},
+  education: {label: 'Education & Teachers', selected: false},
+  food: {label: 'Food & Drink', selected: false},
+  garden: {label: 'Gardening', selected: false},
+  health: {label: 'Health & Fitness', selected: false},
+  history: {label: 'History & Reference', selected: false},
+  lego: {label: 'Lego&reg; Books', selected: false},
+  marvel: {label: 'Marvel', selected: false},
+  pregnancy: {label: 'Pregnancy & Parenting', selected: false},
+  star: {label: 'Star Wars&trade;', selected: false},
+  travel: {label: 'Travel', selected: false}
 };
 
 function init() {
@@ -22,10 +22,10 @@ function init() {
 
   //create click event listners for all buttons
   Object.keys(catergories).forEach(function(key) {
-    document.querySelector('.'+key).addEventListener('click', ()=>{
-      console.log(key);
+    document.querySelector('.'+key).addEventListener('change', ()=>{
+      catergories[key].selected = !catergories[key].selected;
+      console.log('cat: ', key,',', 'selected: ', catergories[key].selected);
     });
-    console.log('.'+key);
   });
 
 }
